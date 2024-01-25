4489f0e23c7ba7a631444c235bd84f2aaaf9129d
sht2017 <1844063767@qq.com>
2024-01-25 14:14:54 -0700

    upd

--- deploy.py
+++ deploy.py
@@ -92,7 +92,7 @@ class DeployKit:
         with open(_RC_LOCAL_PATH, "r+") as file:
             _content = file.read()
             _content = _content.replace("\nexit 0", "")
-            _content = file.read().replace(f"\nscreen -dmS otp-service {_OTP_PATH}/.venv/bin/python3 {_OTP_PATH}/otp.py","")
+            _content = _content.replace(f"\nscreen -dmS otp-service {_OTP_PATH}/.venv/bin/python3 {_OTP_PATH}/otp.py","")
             file.seek(0)
             file.write(f"{_content}\nscreen -dmS otp-service {_OTP_PATH}/.venv/bin/python3 {_OTP_PATH}/otp.py\nexit 0")
         logging.debug("rc.local is fine")
