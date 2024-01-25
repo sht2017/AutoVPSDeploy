bdc8053789d34d9febfbfb3c5357000960b77b65
sht2017 <1844063767@qq.com>
2024-01-25 13:25:54 -0700

    upd

--- install.sh
+++ install.sh
@@ -7,12 +7,11 @@ apt-get install git screen python3 python3-pip python3-venv -y
 ########## [-] Upgrade software packages and install necessary packages [-] ##########
 
 ########## [+] Clone source [+] ##########
-cd ~
-git clone https://github.com/sht2017/AutoVPSDeploy.git
+git clone https://github.com/sht2017/AutoVPSDeploy.git ~/AutoVPSDeploy
 ########## [-] Clone source [-] ##########
 
 ########## [+] Initialization [+] ##########
-cd AutoVPSDeploy
+cd ~/AutoVPSDeploy
 ########## [-] Initialization [-] ##########
 
 ########## [+] Install necessary python libs [+] ##########
